9e43b6efbcb67fbd7149e3d7a46877cc53ffd9f4
Treyul <emmanuelryley55@gmail.com>
2023-02-10 21:31:16 +0300

    added super user and created login forms

--- stockanalysis/settings.py
+++ stockanalysis/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "login"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'stockanalysis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# default redirect when user logins
+LOGIN_REDIRECT_URL = "/"
--- stockanalysis/urls.py
+++ stockanalysis/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("login/signup"),
+    path("login/",include("django.contrib.auth.urls"))
+
 ]

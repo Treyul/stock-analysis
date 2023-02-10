64499fdf5615b1fcdd52c1c71d6ce1fe0938eaf7
Treyul <emmanuelryley55@gmail.com>
2023-02-10 22:41:30 +0300

    initialize apps

---
+++ login/form.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# create user account form
+
+# class New_user_form(UserCreationForm):
+    
